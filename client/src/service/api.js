8a6dbeb2ced2e9d1c9798c5802106cb0ff5dfcb2
Sonali Singh <sonalisingh4275@gmail.com>
2025-06-20 16:28:13 +0530

    Connected frontend to backend using env

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8000';
+const url = 'process.env.https://flipkart-backend-n9vu.onrender.com';
 
 export const authenticateLogin = async (user) => {
     try {
--- server/paytm/PaytmChecksum.js
+++ server/paytm/PaytmChecksum.js
@@ -1,94 +1,98 @@
-"use strict";
+import crypto from 'crypto';
 
-var crypto = require('crypto');
-
-class PaytmChecksum {
+export class PaytmChecksum {
+	static iv = '@@@@&&&&####$$$$';
 
 	static encrypt(input, key) {
-		var cipher = crypto.createCipheriv('AES-128-CBC', key, PaytmChecksum.iv);
-		var encrypted = cipher.update(input, 'binary', 'base64');
+		const cipher = crypto.createCipheriv('AES-128-CBC', key, PaytmChecksum.iv);
+		let encrypted = cipher.update(input, 'binary', 'base64');
 		encrypted += cipher.final('base64');
 		return encrypted;
 	}
+
 	static decrypt(encrypted, key) {
-		var decipher = crypto.createDecipheriv('AES-128-CBC', key, PaytmChecksum.iv);
-		var decrypted = decipher.update(encrypted, 'base64', 'binary');
+		const decipher = crypto.createDecipheriv('AES-128-CBC', key, PaytmChecksum.iv);
+		let decrypted = decipher.update(encrypted, 'base64', 'binary');
 		try {
 			decrypted += decipher.final('binary');
-		}
-		catch (e) {
-			console.log(e);
+		} catch (e) {
+			console.error('Decryption error:', e);
 		}
 		return decrypted;
 	}
-	static generateSignature(params, key) {
-		if (typeof params !== "object" && typeof params !== "string") {
-			var error = "string or object expected, " + (typeof params) + " given.";
-			return Promise.reject(error);
+
+	static async generateSignature(params, key) {
+		if (typeof params !== 'object' && typeof params !== 'string') {
+			throw new Error(`string or object expected, ${typeof params} given.`);
 		}
-		if (typeof params !== "string"){
+		if (typeof params !== 'string') {
 			params = PaytmChecksum.getStringByParams(params);
 		}
 		return PaytmChecksum.generateSignatureByString(params, key);
 	}
-	
 
-	static verifySignature(params, key, checksum) {
-		if (typeof params !== "object" && typeof params !== "string") {
-		   	var error = "string or object expected, " + (typeof params) + " given.";
-			return Promise.reject(error);
+	static async verifySignature(params, key, checksum) {
+		if (typeof params !== 'object' && typeof params !== 'string') {
+			throw new Error(`string or object expected, ${typeof params} given.`);
 		}
-		if(params.hasOwnProperty("CHECKSUMHASH")){
-			delete params.CHECKSUMHASH
+		if (params.hasOwnProperty('CHECKSUMHASH')) {
+			delete params.CHECKSUMHASH;
 		}
-		if (typeof params !== "string"){
+		if (typeof params !== 'string') {
 			params = PaytmChecksum.getStringByParams(params);
 		}
 		return PaytmChecksum.verifySignatureByString(params, key, checksum);
 	}
 
 	static async generateSignatureByString(params, key) {
-		var salt = await PaytmChecksum.generateRandomString(4);
+		const salt = await PaytmChecksum.generateRandomString(4);
 		return PaytmChecksum.calculateChecksum(params, key, salt);
 	}
 
-	static verifySignatureByString(params, key, checksum) {		
-		var paytm_hash = PaytmChecksum.decrypt(checksum, key);
-		var salt = paytm_hash.substr(paytm_hash.length - 4);
-		return (paytm_hash === PaytmChecksum.calculateHash(params, salt));
+	static verifySignatureByString(params, key, checksum) {
+		const paytm_hash = PaytmChecksum.decrypt(checksum, key);
+		const salt = paytm_hash.substr(paytm_hash.length - 4);
+		return paytm_hash === PaytmChecksum.calculateHash(params, salt);
 	}
 
 	static generateRandomString(length) {
-		return new Promise(function (resolve, reject) {
-			crypto.randomBytes((length * 3.0) / 4.0, function (err, buf) {
-				if (!err) {
-					var salt = buf.toString("base64");
-					resolve(salt);					
-				}
-				else {
-					console.log("error occurred in generateRandomString: " + err);
+		return new Promise((resolve, reject) => {
+			crypto.randomBytes((length * 3) / 4, (err, buf) => {
+				if (err) {
+					console.log('generateRandomString error:', err);
 					reject(err);
+				} else {
+					resolve(buf.toString('base64'));
 				}
 			});
 		});
 	}
 
 	static getStringByParams(params) {
-		var data = {};
-		Object.keys(params).sort().forEach(function(key,value) {
-			data[key] = (params[key] !== null && typeof params[key] === 'string' && params[key].toLowerCase() !== "null") ? params[key] : "";
-		});
+		const data = {};
+		Object.keys(params)
+			.sort()
+			.forEach((key) => {
+				data[key] =
+					params[key] !== null &&
+					typeof params[key] === 'string' &&
+					params[key].toLowerCase() !== 'null'
+						? params[key]
+						: '';
+			});
 		return Object.values(data).join('|');
 	}
 
-	static calculateHash(params, salt) {		
-		var finalString = params + "|" + salt;
+	static calculateHash(params, salt) {
+		const finalString = params + '|' + salt;
 		return crypto.createHash('sha256').update(finalString).digest('hex') + salt;
 	}
-	static calculateChecksum(params, key, salt) {		
-		var hashString = PaytmChecksum.calculateHash(params, salt);
-		return PaytmChecksum.encrypt(hashString,key);
+
+	static calculateChecksum(params, key, salt) {
+		const hashString = PaytmChecksum.calculateHash(params, salt);
+		return PaytmChecksum.encrypt(hashString, key);
 	}
 }
-PaytmChecksum.iv = '@@@@&&&&####$$$$';
-module.exports = PaytmChecksum;
+
+// 👇 This makes the whole class available as default
+export default PaytmChecksum;
